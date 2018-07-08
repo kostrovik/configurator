62bed8e1333ef67520cd0c754fdac0f333978d5c
kostrovik <takashi-san@yandex.ru>
2018-07-08 11:48:50 +0300

    GMConfig_3 добавил настройки конфигуратора для чтения кирилицы

---
+++ src/test/java/core/ModuleConfiguratorTest.java
@@ -0,0 +1,36 @@
+package core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * project: configurator
+ * author:  kostrovik
+ * date:    08/07/2018
+ * github:  https://github.com/kostrovik/configurator
+ */
+public class ModuleConfiguratorTest {
+    private String configFile = "test_config.properties";
+    private URL configUrl;
+
+    @BeforeEach
+    void initProperties() {
+        configUrl = this.getClass().getClassLoader().getResource(configFile);
+    }
+
+    @Test
+    void cyrillicPropertyValueTest() {
+        assertNotNull(configUrl);
+
+        ModuleConfigurator configurator = new ModuleConfigurator(configUrl.getPath());
+        Map<String, Object> config = configurator.getConfig();
+
+        assertEquals("Кирилическое название", config.get("prop"));
+    }
+}
